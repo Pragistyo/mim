da5c50b29ef545d291e81ef3afc8f08927dfce47
sisinduku <saptanto.sindu@gmail.com>
2017-10-27 10:22:08 +0700

    Add GET posting API
    URI
    http://<site-url>/<page>/<number_posts>/<userId?>
    Note:
    <site-url>: URL of site API (required)
    <page>: page of posting (required)
    <number_posts>: Number of postings (required)
    <userId?>: ID User (optional)

--- server/routes/postingRoute.js
+++ server/routes/postingRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const image = require('../helpers/image');
 const PostingCtrl = require('../controllers/postingCtrl');
 
-router.get('/:userId?', PostingCtrl.getPostings);
+router.get('/:page/:count/:userId?', PostingCtrl.getPostings);
 router.post('/add_post', image.multer.single('image'), (req, res, next) => {
   PostingCtrl.addPosting(req, res, next)
 });
